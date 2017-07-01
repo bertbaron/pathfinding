632c4bfe71d2e8aaf120de47a7b5a5161a5f0f54
Bert Baron <bertusbaron@gmail.com>
2017-07-01 16:22:32 +0200

    Swapsort example performance improvement

--- examples/swapsort/main.go
+++ examples/swapsort/main.go
@@ -3,51 +3,77 @@ package main
 import (
 	"fmt"
 	"github.com/bertbaron/solve"
+	"sort"
 )
 
-type swapState struct {
-	vector []byte
-	cost   float64
-	op     int
+const N = 16
+
+type sortBytes []byte
+func (b sortBytes) Len() int {
+	return len(b)
+}
+func (b sortBytes) Less(i, j int) bool {
+	return b[i] < b[j]
+}
+func (b sortBytes) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
 }
 
-func newSwapState(vector []byte, cost float64, op int) swapState {
-	return swapState{vector, cost, op}
+type swapContext struct {
+	size int
+	goal [N]byte
+}
+
+type swapState struct {
+	context *swapContext
+	vector  [N]byte
+	cost    float64
+	op      int
+}
+
+func (s swapState) String() string {
+	return fmt.Sprintf("%v, %d", s.vector[0:s.context.size], s.op)
+}
+
+func newSwapState(context *swapContext, vector [N]byte, cost float64, op int) swapState {
+	return swapState{context, vector, cost, op}
 }
 
 func swapProblem(initialState []byte) swapState {
-	return newSwapState(initialState, 0.0, -1)
+	if len(initialState) > N {
+		panic(fmt.Sprintf("Maximum size of vector is %v, but found %v", N, len(initialState)))
+	}
+	var array [N]byte
+	for i, v := range initialState {
+		array[i]=v
+	}
+	sorted := array
+	sort.Sort(sortBytes(sorted[0:len(initialState)]))
+	context := &swapContext{len(initialState), sorted}
+	return newSwapState(context, array, 0.0, -1)
 }
 
 // returns a copy of the given vector, where the element at index is swapped with its right neighbour
-func swap(vector []byte, index int) []byte {
-	cp := make([]byte, len(vector), len(vector))
-	copy(cp, vector)
-	cp[index], cp[index + 1] = cp[index + 1], cp[index]
-	return cp
+func swap(vector [N]byte, index int) [N]byte {
+	vector[index], vector[index+1] = vector[index+1], vector[index]
+	return vector
 }
 
 func (v swapState) Id() interface{} {
-	return fmt.Sprintf("%v", v.vector)
+	return v.vector
 }
 
 func (v swapState) Expand() []solve.State {
-	n := len(v.vector) - 1
+	n := v.context.size - 1
 	steps := make([]solve.State, n, n)
 	for i := 0; i < n; i++ {
-		steps[i] = newSwapState(swap(v.vector, i), v.cost + 1.0, i)
+		steps[i] = newSwapState(v.context, swap(v.vector, i), v.cost + 1.0, i)
 	}
 	return steps
 }
 
 func (v swapState) IsGoal() bool {
-	n := len(v.vector) - 1
-	for i := 0; i < n; i++ {
-		if v.vector[i] > v.vector[i + 1] {
-			return false
-		}
-	}
-	return true
+	return v.vector == v.context.goal
 }
 
 func (v swapState) Cost() float64 {
@@ -55,7 +81,20 @@ func (v swapState) Cost() float64 {
 }
 
 func (v swapState) Heuristic() float64 {
-	return 0.0
+	goal := v.context.goal
+	n := v.context.size
+	offset := 0
+	for i:=0; i < n; i++ {
+		value := v.vector[i]
+		for d:=0; d < n; d++ {
+			l, r := i - d, i + d
+			if l >= 0 && goal[l] == value || r < n && goal[r] == value {
+				offset += d
+				break
+			}
+		}
+	}
+	return float64(offset / 2)
 }
 
 func printSolution(node solve.Node) {
@@ -64,7 +103,7 @@ func printSolution(node solve.Node) {
 	}
 	printSolution(node.Parent())
 	swapstate := node.State().(swapState)
-	for i, e := range swapstate.vector {
+	for i, e := range swapstate.vector[0:swapstate.context.size] {
 		if i > 0 {
 			if i == swapstate.op + 1 {
 				fmt.Print("x")
@@ -78,12 +117,11 @@ func printSolution(node solve.Node) {
 }
 
 func main() {
-	state := swapProblem([]byte{5, 7, 4, 3, 6, 2, 1})
+	state := swapProblem([]byte{7, 6, 5, 4, 3, 2, 1, 0})
 	fmt.Printf("Sorting %v in minimal number of swaps of neighbouring elements\n", state)
 	solution := solve.NewSolver(state).
 		Algorithm(solve.IDAstar).
 		Constraint(solve.CHEAPEST_PATH).
-		Limit(20).
 		Solve()
 
 	fmt.Printf("visited: %d, expanded %d\n", solution.Visited, solution.Expanded)
